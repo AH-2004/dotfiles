553e6f44355df0b5a67d647c54909fb72a6b7831
AH-2004 <ahmedshuaib2004@gmail.com>
2023-02-09 00:56:33 +0500

    Updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,10 +14,7 @@
 (tooltip-mode -1)
 ;; (global-display-line-numbers-mode t)
 (setq user-dialog-box nil)
-
-;; Window size
-(when window-system (set-frame-size (selected-frame) 160 45))
-
+(setq frame-title-format '("Emacs " emacs-version))
 ;; Editing Tweaks
 (setq-default tab-width 4)
 (electric-pair-mode t)
@@ -30,21 +27,25 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;; (load-theme 'tron-legacy t)
 (load-theme 'dracula t)
+
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
 ;; Sidebar
+(use-package treemacs)
+(use-package neotree)
 
 ;; Treemacs
 (use-package treemacs
   :config
-  (progn
-	(setq treemacs-is-never-other-window t)
-	(setq treemacs-width-is-locked nil)
-	(setq treemacs-width-is-initially-locked nil)
-  )
+  (setq treemacs-is-never-other-window t)
+  (setq treemacs-width-is-locked nil)
+  (setq treemacs-width-is-initally-locked nil)
   (treemacs-resize-icons 16)
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
@@ -53,27 +54,29 @@
 (with-eval-after-load 'treemacs (define-key treemacs-mode-map [mouse-1] 'treemacs-single-click-expand-action))
 
 ;; Neotree
-(setq neo-window-fixed-size nil)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-(setq neo-smart-open t)
-(setq neo-show-hidden-files t)
-(setq neo-hide-cursor t)
-(setq neo-autorefresh t)
+(use-package neotree
+  :config
+  (setq neo-window-fixed-size nil)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-smart-open t)
+  (setq neo-show-hidden-files t)
+  (setq neo-hide-cursor t)
+  (setq neo-autorefresh t)
+)
+
+(use-package corfu
+  :config
+  (setq corfu-auto t)
+)
 
 ;; Keybindings
 
-(global-set-key (kbd "C-K") 'neotree-toggle)
-;; (global-set-key (kbd "C-K") 'treemacs)
+;; Sidebar toggling
+(global-set-key (kbd "C-K") 'treemacs)
+;; (global-set-key (kbd "C-K") 'neotree-toggle)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(projectile all-the-icons neotree treemacs)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;Completions
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "C-SPC") 'dabbrev-expand)
+
+(setq custom-file (concat user-emacs-directory "/custom.el"))
