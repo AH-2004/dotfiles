c97b978ab2d6d593ab6dbc4ddd6d5f17ec911406
AH-2004 <ahmedshuaib2004@gmail.com>
2023-02-19 21:32:42 +0500

    Updated tint2 panel config

--- tint2/scripts/ssid.sh
+++ tint2/scripts/ssid.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/sh
-# SSID=$(nmcli -t -f name connection show --active | head -n 1)
-
-# if [[ $SSID ==  "" ]]; then
-#     echo "<txt></txt>"
-#  else
-#     echo "<txt>$SSID</txt>"
-# fi
-
-# echo $SSID
-
-while true
-do
-	echo $(nmcli -t -f name connection show --active | head -n 1)
-done
+SSID=$(nmcli -t -f name connection show --active | head -n 1)
+echo $SSID
