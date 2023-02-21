3c7d2859b3384e1154f587b1753208e67cbe4552
AH-2004 <ahmedshuaib2004@gmail.com>
2023-02-21 12:20:06 +0500

    Test commit

---
+++ .emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(sudo-edit vertigo vertico use-package treemacs projectile neotree multi-vterm lsp-ui kind-icon good-scroll dired-sidebar corfu all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
