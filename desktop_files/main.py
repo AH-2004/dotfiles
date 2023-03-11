606c37a5925ab4380b4971a7907081d8e448d2d0
AH-2004 <ahmedshuaib2004@gmail.com>
2023-03-11 22:51:52 +0500

    Nothing new tbh.

--- desktop_files/main.py
+++ desktop_files/main.py
@@ -1,14 +1,12 @@
-import os
-import glob
+import os.path as path
+from glob import glob
 
 # Desktop file locations
 
 applications = '/usr/share/applications/*.desktop'
-user_applications = f'{os.path.expanduser("~")}/.local/share/applications/*.desktop'
+user_applications = f'{path.expanduser("~")}/.local/share/applications/*.desktop'
 
-# application_dirs_list = os.listdir(applications)+os.listdir(user_applications)
-
-list = glob.glob(applications) + glob.glob(user_applications)
+list = glob(applications) + glob(user_applications)
 
 for i, item in enumerate(list):
     print(i, ": ", item)
