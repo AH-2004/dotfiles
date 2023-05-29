9d0370039770e05d3cab96bcb0f5428753b299a4
AH-2004 <ahmedshuaib2004@gmail.com>
2023-05-29 23:25:10 +0500

    Added variable for changing refreshrate.

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -5,7 +5,8 @@ static const unsigned int borderpx = 3; // border pixel of windows
 static const unsigned int snap = 16; // snap pixel
 static const unsigned int gappx = 6; // gap pixel between windows
 static const int showbar = 1; // 0 means no bar
-static const int topbar = 1; // 0 means bottom bar
+static const int topbar = 1;  // 0 means bottom bar
+static const int refreshrate = 240; // monitor refreshrate
 static const char *fonts[] = { "IBM Plex Mono:size=10" };
 static const char dmenufont[] = "IBM Plex Mono:size=10";
 static const unsigned int baralpha = 0xd0;
