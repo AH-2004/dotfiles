b4ef19082963bd3ee64936ca6fd4888d737e3f13
AH-2004 <ahmedshuaib2004@gmail.com>
2023-06-08 19:47:29 +0500

    repatched dwm with patches and added proper systray support

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -1,19 +1,18 @@
 // See LICENSE file for copyright and license details.
 
 // Appearance
-static const unsigned int borderpx = 3; // border pixel of windows
-static const unsigned int snap = 16; // snap pixel
-static const unsigned int gappx = 12; // gap pixel between windows
-static const int showbar = 1; // 0 means no bar
-static const int topbar = 1;  // 0 means bottom bar
-static const int splitstatus = 1; // 1 for split status items
-static const char *splitdelim = ";"; // Character used for separating status 
-static const int refreshrate = 240; // monitor refreshrate
-static const char *fonts[] = { "IBM Plex Mono:size=9", "Material Icons:size=10" };
+static const unsigned int borderpx = 3;
+static const unsigned int snap = 16;
+static const unsigned int gappx = 12;
+static const int showbar = 1;
+static const int topbar = 1;
+static const int splitstatus = 1;
+static const char *splitdelim = ";"; 
+static const int refreshrate = 240;
+
+// Fonts
+static const char *fonts[] = { "IBM Plex Mono:size=9" };
 static const char dmenufont[] = "IBM Plex Mono:size=9";
-static const unsigned int baralpha = OPAQUE;
-static const unsigned int borderalpha = OPAQUE;
-static const int focusonwheel = 1;
 
 // Colors
 static const char col_gray1[] = "#222222";
@@ -21,98 +20,102 @@ static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_magenta[] = "#bd93f9";
-static const char col_teal[] = "#008080";
+// static const char col_teal[] = "#008080";
 
 static const char *colors[][3] = {
-	// fg, bg, border
+	// { fg, bg, border }
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_magenta, col_magenta }
 };
 
-static const unsigned int alphas[][3] = {
-    // fg, bg, border
-    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-    [SchemeSel] = { OPAQUE, baralpha, borderalpha },
-};
+// Others
+static const int showsystray = 1; 
+static const unsigned int systraypinning = -1; // 0: Selected monitor, < 0: Display
+static const unsigned int systrayonleft = 0;
+static const unsigned int systrayspacing = 6;
+static const unsigned int systrayiconsize = 14;
+static const int systraypinningfailfirst = 1; // 1: First monitor, 0: Last Monitor
+static const int focusonwheel = 1;
+static const int raiseonfocus = 1;
+
+// Alt Tab
+static const unsigned int tabModKey = 0x40;
+static const unsigned int tabCycleKey = 0x17;
+static const unsigned int tabPosY = 1; // 0: Bottom, 1: Center, 2: top
+static const unsigned int tabPosX = 1; // 0: Left, 1: Center, 2: Right
+static const unsigned int maxWTab = 600;
+static const unsigned int maxHTab = 200;
 
 // Tagging
 static const char *tags[] = { "1", "2", "3", "4" };
-
 static const Rule rules[] = {
-	// class, instance, title, tags mask, isfloating, monitor
-	{ NULL, NULL, NULL, 0, 1, -1 }
+	// { class, instance, title, tags mask, isfloating, monitor, unmanaged }
+	{ NULL, NULL, NULL, 0, 1, -1, 0 }
 };
 
 // Layouts
-static const float mfact = 0.55; // factor of master area size [0.05..0.95]
-static const int nmaster = 1; // number of clients in master area
+static const float mfact = 0.55; // Factor of master area size [0.05..0.95]
+static const int nmaster = 1;// Number of clients in master area
 static const int resizehints = 1; // 1 means respect size hints in tiled resizals
-static const int lockfullscreen = 1; // 1 will force focus on the fullscreen window
-
+static const int lockfullscreen = 1; // Force lock fullscreen
 static const Layout layouts[] = {
-	// symbol, arrange function
-	// first entry is default
+	// { symbol, arrange function }
+	// First index is default, NULL means floating
 	{ "Tile", tile },
-	{ "Float", NULL }, // no layout function means floating behavior
-	{ "Mono", monocle }
+	{ "Float", NULL },
+	{ "[M]", monocle }
 };
 
-// Key Definitions
+// Definitions
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY, KEY, view, {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask, KEY, toggleview, {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask, KEY, tag, {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY, toggletag, {.ui = 1 << TAG} },
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-// helper for spawning shell commands in the pre dwm-5.0 fashion
+// Helper for spawning shell commands in the pre dwm-5.0 fashion
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+static char dmenumon[2] = "0"; // Component of dmenucmd, manipulated in spawn()
 
 // Commands
-static char dmenumon[2] = "0"; // component of dmenucmd, manipulated in spawn()
 static const char *dmenucmd[] = { "j4-dmenu-desktop", "--dmenu=dmenu", "--no-generic", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *suspendcmd[]  = { "systemctl", "suspend", NULL };
+static const char *termcmd[] = { "alacritty", NULL };
+static const char *suspendcmd[] = { "systemctl", "suspend", NULL };
 
+// Keys and Buttons
 static const Key keys[] = {
-	// modifier, key, function, argument
-	{ Mod1Mask, XK_space, spawn, {.v = dmenucmd } },
-	{ Mod1Mask|ControlMask, XK_t, spawn, {.v = termcmd } },
-	{ MODKEY, XK_l, spawn, {.v = suspendcmd } },
-	{ MODKEY, XK_b, togglebar, {0} },
-	
-	{ MODKEY, XK_j, focusstack, {.i = +1 } },
-	{ MODKEY, XK_k, focusstack, {.i = -1 } },
-	{ MODKEY, XK_i, incnmaster, {.i = +1 } },
-	{ MODKEY, XK_d, incnmaster, {.i = -1 } },
-	
-	{ MODKEY, XK_Left, setmfact, {.f = -0.05} },
-	{ MODKEY, XK_Right, setmfact, {.f = +0.05} },
-	{ MODKEY, XK_Return, zoom, {0} },
-	{ MODKEY, XK_Tab, view, {0} },
-	{ MODKEY, XK_x, killclient, {0} },
-	{ MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
-	{ MODKEY, XK_f, setlayout, {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask, XK_space, togglefloating, {0} },
-	{ MODKEY, XK_0, view, {.ui = ~0 } },
-	{ MODKEY|ShiftMask, XK_0, tag, {.ui = ~0 } },
-	{ MODKEY, XK_comma, focusmon, {.i = -1 } },
-	{ MODKEY, XK_period, focusmon, {.i = +1 } },
-	{ MODKEY|ShiftMask, XK_comma, tagmon, {.i = -3 } },
-	{ MODKEY|ShiftMask, XK_period, tagmon, {.i = +3 } },
-	{ MODKEY, XK_minus,  setgaps, {.i = -3 } },
-	{ MODKEY, XK_equal, setgaps, {.i = +3 } },
-	{ MODKEY|ShiftMask, XK_q, quit, {0} },
-	TAGKEYS(XK_1, 0)
-	TAGKEYS(XK_2, 1)
-	TAGKEYS(XK_3, 2)
-	TAGKEYS(XK_4, 3)
-};
+    // { modifier, key, function, argument }
+    {Mod1Mask, XK_space, spawn, {.v = dmenucmd}},
+    {Mod1Mask | ControlMask, XK_t, spawn, {.v = termcmd}},
+    {MODKEY, XK_l, spawn, {.v = suspendcmd}},
+    {MODKEY, XK_b, togglebar, {0}},
+    {MODKEY, XK_j, focusstack, {.i = +1}},
+    {MODKEY, XK_k, focusstack, {.i = -1}},
+    {MODKEY, XK_i, incnmaster, {.i = +1}},
+    {MODKEY, XK_d, incnmaster, {.i = -1}},
+    {MODKEY, XK_Left, setmfact, {.f = -0.05}},
+    {MODKEY, XK_Right, setmfact, {.f = +0.05}},
+    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY, XK_x, killclient, {0}},
+    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
+    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+    {MODKEY, XK_0, view, {.ui = ~0}},
+    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+    {MODKEY, XK_comma, focusmon, {.i = -1}},
+    {MODKEY, XK_period, focusmon, {.i = +1}},
+    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -3}},
+    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +3}},
+    {MODKEY, XK_minus, setgaps, {.i = -3}},
+    {MODKEY, XK_equal, setgaps, {.i = +3}},
+    {Mod1Mask, XK_Tab, altTabStart, {0}},
+    {MODKEY | ShiftMask, XK_q, quit, {0}},
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)};
 
-// Button Definitions
-// click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin
 static const Button buttons[] = {
-	// click, event mask, button, function, argument
+	// click:  ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, ClkRootWin
+	// { click, event mask, button, function, argument }
 	{ ClkLtSymbol, 0, Button1, setlayout, {0} },
 	{ ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]} },
 	{ ClkWinTitle, 0, Button2, zoom, {0} },
